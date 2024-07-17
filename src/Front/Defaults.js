ce2fee217ecb23e56507b0d9118df65511dc0f44
Alex Gusev <alex@flancer64.com>
2024-07-17 10:20:04 +0300

    pwa-wallet-2 Define the basic functionality and routes
    link to issue: flancer64/pwa-wallet#2

--- src/Front/Defaults.js
+++ src/Front/Defaults.js
@@ -4,9 +4,17 @@
 export default class Wallet_Front_Defaults {
 
     ROUTE_ABOUT = '/about';
+    ROUTE_APP_EXPORT = '/app/export';
+    ROUTE_APP_IMPORT = '/app/import';
     ROUTE_APP_UPGRADE = '/app/upgrade';
+    ROUTE_CARD_ADD = '/card/add';
+    ROUTE_CARD_EDIT_X = '/card/edit/:uuid';
+    ROUTE_CARD_LIST = '/card/list';
+    ROUTE_CARD_USE_X = '/card/use/:uuid';
     ROUTE_HOME = '/';
+    ROUTE_PLACE_LIST = '/place/list';
     ROUTE_SETTINGS = '/settings';
+    ROUTE_TAG_LIST = '/tag/list';
 
     /** @type {Wallet_Shared_Defaults} */
     SHARED;
